2473992ccc7c7ecc3cba02926e7d5f5cf2c487d5
Onuralp AVCI <onuralp20002@gmail.com>
2021-12-20 22:10:28 +0300

    Return all fields of club manager in club list

--- django/src/drf/clubs/serializers.py
+++ django/src/drf/clubs/serializers.py
@@ -1,8 +1,10 @@
 from rest_framework import serializers
+from accounts.serializers import AccountSerializer
 from .models import Club
 
 # Club Serializer
 class ClubSerializer(serializers.ModelSerializer):
+    owner = AccountSerializer(read_only=True)
     class Meta:
         model = Club
         fields = '__all__'
