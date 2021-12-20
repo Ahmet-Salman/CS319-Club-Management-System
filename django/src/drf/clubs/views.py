663d9d265bab4de31559440388ac9ac1ed3a3c37
Onuralp AVCI <onuralp20002@gmail.com>
2021-12-20 23:55:07 +0300

    Fix serializer for post method of clubs

--- django/src/drf/clubs/views.py
+++ django/src/drf/clubs/views.py
@@ -1,7 +1,7 @@
 # Create your views here.
 
-from rest_framework import generics
-from .serializers import ClubSerializer
+from rest_framework import generics, permissions
+from .serializers import ReadClubSerializer, WriteClubSerializer
 from .models import Club
 from accounts.permissions import IsAdminOrReadOnly
 from .permissions import IsClubOwnerOrReadOnly
@@ -9,10 +9,17 @@ from .permissions import IsClubOwnerOrReadOnly
 class ClubList(generics.ListCreateAPIView):
     permission_classes = [IsAdminOrReadOnly]
     queryset = Club.objects.all()
-    serializer_class = ClubSerializer
+    def get_serializer_class(self):
+        if self.request.method in permissions.SAFE_METHODS:
+            return ReadClubSerializer
+        return WriteClubSerializer
+
 
 class ClubDetails(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = [IsClubOwnerOrReadOnly]
-    serializer_class = ClubSerializer
     lookup_url_kwarg = 'id'
     queryset = Club.objects.all()
+    def get_serializer_class(self):
+        if self.request.method in permissions.SAFE_METHODS:
+            return ReadClubSerializer
+        return WriteClubSerializer
