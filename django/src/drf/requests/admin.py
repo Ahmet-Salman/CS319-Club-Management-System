7ec35e5b00af7dacc0203d8dcb8211c1b8880309
Onuralp AVCI <onuralp20002@gmail.com>
2021-12-21 10:48:41 +0300

    register all requests to django admin

--- django/src/drf/requests/admin.py
+++ django/src/drf/requests/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
-from .models import CreateClubRequest
+from .models import CreateClubRequest, JoinClubRequest, DeleteClubRequest
 
 # Register your models here.
 admin.site.register(CreateClubRequest)
+admin.site.register(JoinClubRequest)
+admin.site.register(DeleteClubRequest)
