627598e8e6e5c9032d74ad50b1ced4839dbc3e50
alperen59 <63664429+alperen59@users.noreply.github.com>
2021-12-23 20:12:11 +0300

    final

--- django/src/drf/club_enrollment/serializers.py
+++ django/src/drf/club_enrollment/serializers.py
@@ -1,7 +1,9 @@
 from rest_framework import serializers
 from .models import ClubEnrollment
+from accounts.serializers import AccountSerializer
 
 class ClubEnrollmentSearializer(serializers.ModelSerializer):
+    user_id = AccountSerializer(read_only=True)
     class Meta:
         model = ClubEnrollment
         fields = '__all__'
