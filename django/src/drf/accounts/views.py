a5d03402acacf41c9b9305ba3d4bc7fbc2ece295
Onuralp AVCI <onuralp20002@gmail.com>
2021-12-24 09:29:56 +0300

    Add change password endpoint

--- django/src/drf/accounts/views.py
+++ django/src/drf/accounts/views.py
@@ -11,7 +11,7 @@ from rest_framework import status
 from rest_framework import generics
 
 from .models import Account
-from .serializers import AccountSerializer
+from .serializers import AccountSerializer, ChangePasswordSerializer
 from django.http import Http404
 
 class AccountList(APIView):
@@ -70,3 +70,37 @@ class LogoutAPI(APIView):
         request.user.auth_token.delete()
         logout(request)
         return Response(serializer.data)
+
+class ChangePasswordView(generics.UpdateAPIView):
+    """
+    An endpoint for changing password.
+    """
+    serializer_class = ChangePasswordSerializer
+    model = Account
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def get_object(self, queryset=None):
+        obj = self.request.user
+        return obj
+
+    def update(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        serializer = self.get_serializer(data=request.data)
+
+        if serializer.is_valid():
+            # Check old password
+            if not self.object.check_password(serializer.data.get("old_password")):
+                return Response({"old_password": ["Wrong password."]}, status=status.HTTP_400_BAD_REQUEST)
+            # set_password also hashes the password that the user will get
+            self.object.set_password(serializer.data.get("new_password"))
+            self.object.save()
+            response = {
+                'status': 'success',
+                'code': status.HTTP_200_OK,
+                'message': 'Password updated successfully',
+                'data': []
+            }
+
+            return Response(response)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
